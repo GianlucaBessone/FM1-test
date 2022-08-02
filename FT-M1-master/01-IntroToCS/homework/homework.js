6b9a7b29218cb4792f506e7cba19f001407bfb4e
GianlucaBessone <bessone.gianluca@gmail.com>
2022-08-02 12:01:00 -0300

    01 - 100%

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,17 +1,25 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  const binary = num.toString().split("");
+  const xreverseBinary = binary.reverse();
+  let acc = 0;
+  const xtransform = binary.map((v, k) => (acc += v * Math.pow(2, k)), acc);
+  return acc;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let acc = "";
+  while (num !== 0) {
+    acc = (num % 2) + acc;
+    num = Math.floor(num / 2);
+  }
+  return acc;
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
