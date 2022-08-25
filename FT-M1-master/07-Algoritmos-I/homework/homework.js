748665ac3cd08755bf78da41af072e8d52b56b31
GianlucaBessone <bessone.gianluca@gmail.com>
2022-08-25 20:50:46 -0300

    07 Completed

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -6,11 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-  function nFactorial(n) {
-    if (n <= 0) return 0;
-    if (n === 1) return 1;
-    if (n > 1) return n * nFactorial(n - 1);
+  let result = [1];
+  let val = 2;
+  while (num > 1) {
+    if (num % val === 0) {
+      result.push(val);
+      num = num / val;
+    } else {
+      val++;
+    }
   }
+  return result;
 }
 
 function bubbleSort(array) {
@@ -18,6 +24,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let swap = true;
+  while (swap) {
+    swap = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 function insertionSort(array) {
@@ -25,6 +44,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++) {
+    let x = i - 1;
+    let temp = array[i];
+    while (x >= 0 && temp < array[x]) {
+      array[x + 1] = array[x];
+      x--;
+    }
+    array[x + 1] = temp;
+  }
+  return array;
 }
 
 function selectionSort(array) {
@@ -32,6 +61,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let x = i + 1; x < array.length; x++) {
+      if (array[min] > array[x]) min = x;
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
 }
 
 // No modificar nada debajo de esta línea
